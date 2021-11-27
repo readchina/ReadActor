57cbbbba5eaf6f3af172a455ddd40e37fa97495a
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2021-11-27 02:00:17 +0100

    backup before modify the data structure for storing weights again

--- Python scripts/authenticity_person.py
+++ Python scripts/authenticity_person.py
@@ -41,22 +41,19 @@ def read_person_csv(person_url="https://raw.githubusercontent.com/readchina/Read
     """
     df = pd.read_csv(person_url, error_bad_lines=False)
     person_dict = {}
-    person_dict_with_name_lang = {}
     place_dict = read_space_csv()
     for index, row in df.iterrows():
         # a dictionary to collect final q_id for each unique person id
-        person_dict[row['person_id']] = ""
+        if row['person_id'] not in person_dict:
+            person_dict[row['person_id']] = dict()
 
-        key = (row['person_id'], row['name_lang'])
-        if key not in person_dict_with_name_lang:
-
+        if row['name_lang'] not in person_dict[row['person_id']]:
             # name_ordered is a list of a single name or multiple names
             name_ordered = __order_name_by_language(row)
 
             # sex or gender type in Wikidata for human: male, female, non-binary, intersex, transgender female,
             # transgender male, agender.
-            if row['sex'] not in ['male', 'female', 'non-binary', 'intersex', 'transgender female', 'transgender ',
-                                  'male', 'agender']:
+            if row['sex'] not in ['male', 'female', 'non-binary', 'intersex', 'transgender female', 'transgender ','male', 'agender']:
                 row['sex'] = ""
             else:
                 row['sex'] = row['sex'].strip()
@@ -76,11 +73,10 @@ def read_person_csv(person_url="https://raw.githubusercontent.com/readchina/Read
             else:
                 print("Please check why the place of birth is not in the dictionary of space.")
 
-            person_dict_with_name_lang[key] = [name_ordered, row['sex'], birth_years, death_years, row['alt_name'],
-                                row['place_of_birth']]
+            person_dict[row['person_id']][row['name_lang']] = [name_ordered, row['sex'], birth_years, death_years, row['alt_name'], row['place_of_birth']]
         else:
-            print("Please check why the key is repeated. ")
-    return person_dict_with_name_lang, person_dict
+            print("Please check why the combination of person id and name_lang is repeated. ")
+    return person_dict
 
 
 def __order_name_by_language(row):
@@ -129,71 +125,55 @@ def __clean_birth_death_year_format(default_year):
     return years
 
 
-def compare(person_dict_with_name_lang, person_dict, sleep=2):
-    for k, v in person_dict_with_name_lang.items():
-        # get the language of the name, to be used as language tag in the SPARQL query
-        person_id = k[0] # k[0]: {k[1]: weight, q-ids}
-        lang = k[1]
+def get_person_weight(person_dict, sleep=2):
+    person_weight_dict = {}
+    for person_id, value in person_dict.items():
+        person_weight_dict[person_id] = []
+        name_langs = value.keys()
+        l = []
+        for lang in name_langs:
+            l.append(lang)
+            v = value[lang]
+            # Use the ordered_name list as lookups
+            lookup_names = v[0]
 
-        # Use the ordered_name list as lookups
-        lookup_names = v[0]
+            # If there are alt_name, add it into the list of names
+            if len(value[lang][4]) != 0:
+                lookup_names.append(v[4])
 
-        # If there are alt_name, add it into the list of names
-        if len(v[4]) != 0:
-            lookup_names.append(v[4])
+            # print("====lookup_names:\n", lookup_names)
+            if lookup_names != ['anonymous'] and lookup_names != ['无名']:
+                person = _sparql(lookup_names, lang, sleep)
+            else:
+                continue
 
-        # print("====lookup_names:\n", lookup_names)
-        if lookup_names != ['anonymous'] and lookup_names != ['无名']:
-            person = _sparql(lookup_names, lang, sleep)
-        else:
-            continue
-
-        if len(person) == 0:
-            print("There is no match for the person unique id ", id, " with language ", lang)
-
-        else:
-            weight = 0
-            weight_Q_pairs = {}
-            for Q_id, p in person.items():
-                # all the matched fields will add weight 1 to the total weight for this Q_id
-                if 'gender' in p:
-                    if p['gender'] == v[1]:
-                        weight += 1
-                elif 'birthyear' in p:
-                    if p['birthyear'] in v[2]:
-                        weight += 1
-                elif 'deathyear' in p:
-                    if p['deathyear'] in v[3]:
-                        weight += 1
-                elif 'birthplace' in p:
-                    if p['birthplace'] == v[5]:
-                        weight += 1
-
-                weight_Q_pairs[Q_id] = weight
-                weight = 0
-            if len(weight_Q_pairs) == 0:
-                print("There is no match for the person unique id ", id, " with language ", lang)
+            if len(person) == 0:
+                print("There is no match for the person unique id ", person_id, " with language ", lang)
 
             else:
-                q_ids = []
-                max_weight = max(weight_Q_pairs.values())
+                weight = 0
+                weight_Q_pairs = {}
+                for Q_id, p in person.items():
+                    # all the matched fields will add weight 1 to the total weight for this Q_id
+                    if 'gender' in p:
+                        if p['gender'] == v[1]:
+                            weight += 1
+                    elif 'birthyear' in p:
+                        if p['birthyear'] in v[2]:
+                            weight += 1
+                    elif 'deathyear' in p:
+                        if p['deathyear'] in v[3]:
+                            weight += 1
+                    elif 'birthplace' in p:
+                        if p['birthplace'] == v[5]:
+                            weight += 1
 
-                for id, w in weight_Q_pairs.items():
-                   if w == max_weight:
-                       q_ids.append(id)
-                if len(q_ids) == 1:
-                    # print("--This person, ", k, v, "should be matched with this Wikidata entity", ids[0],
-                    #       '\nand this entity has the following information: ', person[ids[0]])
-                    # person_dict[person_id] = {lang: [weight, ]}
-                    pass
-                else:
-                    # this part will be used to correct wrong entries for matched person in Person.csv
-                    # therefore, there should be an algorithm to choose the matched one from all the entities which
-                    # all has the hightest weight
-                    # print("--HEY, multiple highest-weight match for a person!") # should be decomment later
-                    pass
+                    l.append([weight, Q_id])
+                    weight = 0
 
-    return no_match
+            person_weight_dict[person_id].append(l)
+            l = []
+    return person_weight_dict
 
 
 def _sparql(lookup_names, lang, sleep=2):
@@ -231,16 +211,81 @@ def _sparql(lookup_names, lang, sleep=2):
             time.sleep(sleep)
     return person
 
+def compare_weights(person_weight_dict):
+    no_match = []
+    person_match_dict = {}
+    for id, value in person_weight_dict.items():
+        # No match at all
+        if len(value) < 1:
+            no_match.append(id)
+            person_match_dict[id] = ""
+        # At least one match for one name_lang, no match for the rest name_lang
+        elif len(person_weight_dict[id]) == 1:
+            lang = person_weight_dict[id][0][0]
+            weights = []
+            for i in person_weight_dict[id][0][1:]:
+                w = i[0]
+
+
+
+            # weight_Qid_list = person_weight_dict[id][lang]
+            # weights = []
+            # for i in weight_Qid_list:
+            #     weight = i[0]
+            #     weights.append(weight)
+            # max_weight = max(weights)
+            # index = weights.index(max_weight) # If there are multiple max values, the first one will be returned
+            # Qid = weight_Qid_list[index][1]
+            # print("Qid: ", Qid)
+            # person_match_dict[id] = Q-id
+        # There are at least one match for each name_lang
+        else:
+            for lang in person_weight_dict[id].keys():
+                Q_ids = []
+                for w, q in person_weight_dict[id][lang].items():
+                    Q_ids.append(q)
+
+
+
+            weights = []
+            max_weight = 0
+            for lang in person_weight_dict[id].keys(): # Multiple iterations
+                weight_Qid_list = person_weight_dict[id][lang]
+                weights = []
+                for i in weight_Qid_list:
+                    weight = i[0]
+                    weights.append(weight)
+                max_weight = max(weights)
+                index = weights.index(max_weight) # If there are multiple max values, the first one will be returned
+                Qid = weight_Qid_list[index][1]
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    person_dict_with_name_lang, person_dict = read_person_csv(
+    person_dict = read_person_csv(
         "https://raw.githubusercontent.com/readchina/ReadAct/master/csv/data/Person.csv")
 
-    # print(person_dict)
+    # for id, lang in person_dict.items():
+    #     print(len(lang))
 
-    sample_dict = { ('AG0009', 'zh'): [['北岛'], 'male', [1949], [], '赵振开', 'Beijing'],('AG0004', 'en'): [['Ke Mang',
-                                                                                                         'Mang Ke'],'male', [1950], [], 'Jiang Shiwei', 'Shenyang'], ('AG0004', 'zh'): [['芒克'], 'male', [1950], [], '姜世伟', 'Shenyang'], ('AG0005', 'en'): [['Gang Peng', 'Peng Gang'], 'male', [1952], [], '', 'Beijing'],('AG0009', 'en'): [['Dao Bei', 'Bei Dao'], 'male', [1949], [], 'Zhao Zhenkai', 'Beijing']}
 
-    no_match = compare(person_dict_with_name_lang, person_dict, 2)
-    print("no_match", no_match)
-    print("-------length of the no_match", len(no_match))
+    sample_dict = {'AG0001': {'en': [['Xun Lu', 'Lu Xun'], 'male', [1881], [1936], 'Zhou Shuren', 'Shaoxing'],
+                              'zh': [['鲁迅'], 'male', [1881], [1936], '周树人', 'Shaoxing']}}
+
+    # , 'AG0089': {'en': [['Konstantin Balmont', 'Balmont Konstantin'], 'male', [1876], [1942], '', 'Shuya'], 'ru': [['Константи́н Бальмо́нт', 'Бальмо́нт Константи́н'], 'male', [1876], [1942], '', 'Shuya'], 'zh': [['巴尔蒙特康斯坦丁'], 'male', [1876], [1942], '', 'Shuya']}, 'AG0090': {'en': [['Honoré de Balzac', 'Balzac Honoré de'], 'male', [1799], [1850], '', 'Tours'], 'zh': [['巴尔扎克奥诺雷·德'], 'male', [1799], [1850], '', 'Tours']}, 'AG0091': {'en': [['Charles Baudelaire', 'Baudelaire Charles'], 'male', [1821], [1867], '', 'Paris'], 'zh': [['波德莱尔夏尔'], 'male', [1821], [1867], '', 'Paris']}, 'AG0092': {'en': [['Samuel Beckett', 'Beckett Samuel'], 'male', [1906], [1989], '', 'Foxrock'], 'zh': [['贝克特萨缪尔'], 'male', [1906], [1989], '', 'Foxrock']}, 'AG0097': {'en': [['Ruxie Bi', 'Bi Ruxie'], 'male', [], [], '', 'unknown'], 'zh': [['毕汝协'], 'male', [], [], '毕汝谐', 'unknown']}, 'AG0098': {'en': [['Zhilin Bian', 'Bian Zhilin'], 'male', [1910], [2000], '', 'Haimen'], 'zh': [['卞之琳'], 'male', [1910], [2000], '', 'Haimen']}, 'AG0511': {'en': [['Er Nie', 'Nie Er'], 'male', [1912], [1935], 'Nie Shouxin', 'Vinci'], 'zh': [['聂耳'], 'male', [1912], [1935], '聂守信', 'Vinci']}
+
+
+    person_weight_dict = get_person_weight(sample_dict, 2)
+
+    print(person_weight_dict)
+
