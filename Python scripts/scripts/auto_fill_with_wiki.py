fe8d03677460b78f4a81b3376b8fdd4871efe01b
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2021-12-16 14:58:39 +0100

    start of the auto-fill

--- Python scripts/scripts/auto_fill_with_wiki.py
+++ Python scripts/scripts/auto_fill_with_wiki.py
@@ -3,6 +3,17 @@ import pandas as pd
 import argparse
 from validate_CSV_columns import validate_csv
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='An auto-fill tool for given person name and Wikipedia link.')
+    parser.add_argument('-i', '--input', dest='input_filename', action='store', default='data.csv', type=str, help='specify the input file, default `data.csv`')
+    parser.add_argument('-o', '--output', dest='output_filename', action='store',default='outliers_result.csv', type=str, help='specify the output file, default `outliers_result.csv`')
+
+
+
+
+
+
+
 
 def autofill():
 
