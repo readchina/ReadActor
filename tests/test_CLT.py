cab85e89ba4f45922ba1de9c642a831a66f76d1d
duncdrum <d.paterson@me.com>
2022-03-31 14:47:38 +0200

    fix: update test name

--- tests/test_CLT.py
+++ tests/test_CLT.py
@@ -12,10 +12,12 @@ class MyTestCase(unittest.TestCase):
         self.l = ['', '', '', '', '', '', '', '', '', '', '', '', '', '']
         self.df = pd.DataFrame([self.l])
         self.df.columns = self.column_names
-        self.df_person_gh = pd.read_csv('CSV/df_person_Github_fake.csv')  # unofficial version
-        self.last_person_id, self.person_ids_gh, self.wikidata_ids_GH = get_last_id(self.df_person_gh)
+        self.df_person_gh = pd.read_csv(
+            'CSV/df_person_Github_fake.csv')  # unofficial version
+        self.last_person_id, self.person_ids_gh, self.wikidata_ids_GH = get_last_id(
+            self.df_person_gh)
 
-    def test_should_skip_expect_no_change(self):
+    def test_it_should_respect_skip_annotation(self):
         self.l = ['AG0001', '鲁', '迅', 'zh', 'male', '0000', '0000', 'Shaoxing', 'Q23114', '2021-12-22', 'QG', '', '',
                   'skip']
         self.df.loc[0] = self.l
@@ -24,7 +26,7 @@ class MyTestCase(unittest.TestCase):
                                                                             '0000', 'Shaoxing', 'Q23114', '2021-12-22',
                                                                             'QG', '', '', 'skip'])
 
-    def test_should_NOT_skip(self):
+    def test_it_should_NOT_skip_without_annotation(self):
         self.l = ['AG0001', '鲁', '迅', 'zh', 'male', '0000', '1111', 'Shaoxing', 'Q23114', '2021-12-22', 'QG', '', '',
                   '']
         self.df.loc[0] = self.l
@@ -33,26 +35,25 @@ class MyTestCase(unittest.TestCase):
                            self.wikidata_ids_GH)[0].tolist(),
             ['AG0001', '鲁', '迅', 'zh', 'male', '1881', '1936', 'Shaoxing', 'Q23114', '2021-12-22', 'QG', '', '', ''])
 
-    def test_should_match_all_expect_no_change(self):
+    def test_it_should_match_all_witout_change(self):
         self.l = ['AG0001', '鲁', '迅', 'zh', 'male', '1881', '1936', 'SP0048', 'Q23114', '2017-07-03', 'LH',
-                  '2020-04-02',
-                  'DP', '']
+                  '2020-04-02', 'DP', '']
         self.df.loc[0] = self.l
         self.assertEqual(check_each_row(0, self.df.iloc[0], self.df_person_gh, self.person_ids_gh, self.last_person_id,
                                         self.wikidata_ids_GH)[0].tolist()[0:-1],
-                         ['AG0001', '鲁', '迅', 'zh', 'male', '1881',
-                          '1936', 'SP0048', 'Q23114', '2017-07-03', 'LH', '2020-04-02', 'DP'])
+                         ['AG0001', '鲁', '迅', 'zh', 'male', '1881', '1936', 'SP0048', 'Q23114', '2017-07-03', 'LH',
+                         '2020-04-02', 'DP'])
 
-    def test_should_two_ids_match_but_other_person_infos_NOT_match_expect_overwrite_with_ReadAct(self):
+    def test_it_should_update_data_for_full_id_match(self):
         self.l = ['AG0001', '鲁', '迅', 'zh', 'male', '1881', '1936', 'Shaoxing', 'Q23114', '2021-12-22', 'QG', '', '',
                   '']
         self.df.loc[0] = self.l
         self.assertEqual(check_each_row(0, self.df.iloc[0], self.df_person_gh, self.person_ids_gh, self.last_person_id,
                                         self.wikidata_ids_GH)[0].tolist()[0:-1],
-                         ['AG0001', '鲁', '迅', 'zh', 'male', '1881',
-                          '1936', 'SP0048', 'Q23114', '2017-07-03', 'LH', '2020-04-02', 'DP'])
+                         ['AG0001', '鲁', '迅', 'zh', 'male', '1881', '1936', 'SP0048', 'Q23114', '2017-07-03', 'LH',
+                          '2020-04-02', 'DP'])
 
-    def test_should_same_personID_has_different_wikiIds_expect_error(self):
+    def test_it_should_fail_on_ID_conflict(self):
         self.l = ['AG0001', '鲁', '迅', 'zh', 'male', '1881', '1936', 'Shaoxing', 'Q00000000', '2021-12-22', 'QG', '', '',
                   '']
         self.df.loc[0] = self.l
@@ -62,7 +63,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(cm.exception.code, "For row0 : Error: `wikidata_id` does not match GitHub data. Please "
                                             "check. By SemBot.")
 
-    def test_should_new_person_but_given_wikiId_already_in_ReadAct_expect_error(
+    def test_it_should_not_reuse_wikiID(
             self):
         self.l = ['AG1200', 'Monet', 'Claude', 'en', 'male', '1840', '1926', 'Paris', 'Q23114', '2021-12-22', 'QG', '',
                   '', '']
@@ -74,19 +75,17 @@ class MyTestCase(unittest.TestCase):
                          "For row0 :Error: `wikidata_id` already exists in GitHub data but the `person_id` does not "
                          "match. Please check.")
 
-    def test_should_two_new_ids_and_person_infos_match_WikiData_infos_expect_no_change(
-            self):
+    # TODO(DP): I do not understand how this testcase is different from the second test above
+    def test_it_should_not_update_new_entries_with_matching_data(self):
         self.l = ['AG1200', 'Monet', 'Claude', 'en', 'male', '1840', '1926', 'Paris', 'Q296', '2021-12-22', 'QG', '',
                   '', '']
         self.df.loc[0] = self.l
         self.assertEqual(check_each_row(0, self.df.iloc[0], self.df_person_gh, self.person_ids_gh, self.last_person_id,
                                         self.wikidata_ids_GH)[0].tolist()[0:-1], ['AG1200', 'Monet', 'Claude', 'en',
                                                                                   'male', '1840', '1926', 'Paris',
-                                                                                  'Q296',
-                                                                                  '2021-12-22', 'QG', '', ''])
+                                                                                  'Q296', '2021-12-22', 'QG', '', ''])
 
-    def test_should_two_new_ids_and_person_infos_NOT_match_WikiData_expect_updating_unmached_cells(
-            self):
+    def test_should_it_should_update_new_entries_with_missing_data(self):
         self.l = ['AG1200', 'Monet', 'Claude', 'en', '', '', '', '', 'Q296', '2021-12-22', 'QG', '',
                   '', '']
         self.df.loc[0] = self.l
@@ -95,9 +94,9 @@ class MyTestCase(unittest.TestCase):
                                                                                   'male', '1840', '1926', 'Paris',
                                                                                   'Q296', '2021-12-22', 'QG', '', ''])
 
-    def test_should_new_person_but_queried_wikiId_already_in_ReadAct_expect_error(
-            self):
-        self.l = ['AG1200', '鲁', '迅', 'zh', '', '', '', '', '', '2021-12-22', 'QG', '', '', '']
+    def test_it_should_use_wikiID_to_dedupe_readActID(self):
+        self.l = ['AG1200', '鲁', '迅', 'zh', '', '',
+                  '', '', '', '2021-12-22', 'QG', '', '', '']
         self.df.loc[0] = self.l
         with self.assertRaises(SystemExit) as cm:
             check_each_row(0, self.df.iloc[0], self.df_person_gh, self.person_ids_gh, self.last_person_id,
@@ -110,8 +109,7 @@ class MyTestCase(unittest.TestCase):
                                             "in Wikidata database. Please put \"skip\" in \"note\" column for this "
                                             "row and run this program again. By SemBot.")
 
-    def test_should_new_person_has_infos_match_WikiData_infos_expect_only_update_WikiId(
-            self):
+    def test_it_should_update_wikiID_using_persondata(self):
         self.l = ['AG1200', 'Monet', 'Claude', 'en', 'male', '1840', '1926', 'Paris', '', '2021-12-22', 'QG', '',
                   '', '']
         self.df.loc[0] = self.l
@@ -120,16 +118,14 @@ class MyTestCase(unittest.TestCase):
                                                                                   'male', '1840', '1926', 'Paris',
                                                                                   'Q296', '2021-12-22', 'QG', '', ''])
 
-    def should_new_person_has_infos_NOT_match_WikiData_expect_update_with_WikiData(
-            self):
+    def test_it_should_update_data_with_missing_wikiID(self):
         self.l = ['AG1200', 'Monet', 'Claude', 'en', 'male', '1840', '1926', 'Tokyo', '', '2021-12-22', 'QG', '',
                   '', '']
         self.df.loc[0] = self.l
         self.assertEqual(check_each_row(0, self.df.iloc[0], self.df_person_gh, self.person_ids_gh, self.last_person_id,
                                         self.wikidata_ids_GH)[0].tolist()[0:-1], ['AG1200', 'Monet', 'Claude', 'en',
                                                                                   'male', '1840', '1926', 'Paris',
-                                                                                  'Q296',
-                                                                                  '2021-12-22', 'QG', '',
+                                                                                  'Q296', '2021-12-22', 'QG', '',
                                                                                   ''])  # should test the warning
         # clean up after testing updated files
 
