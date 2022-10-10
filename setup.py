f82499cee24b34509d830b897ccd61d9581b74b0
duncdrum <d.paterson@me.com>
2022-10-10 15:11:19 +0200

    🐳 chore(setup): dev version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ReadActor",
-    version="2.0.1",
+    version="2.0.2-alpha",
     author="Qin Gu",
     author_email="guqin7@gmail.com",
     description="A lookup tool for ReadChina project",
