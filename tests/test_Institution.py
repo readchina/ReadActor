6aea5b1b4776175e88ef610272f1536687ae3d65
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2022-10-25 01:17:19 +0200

    clean up 2.5/8
    Using external data in unit tests.
    Start with Institution.
    There should be one test for each leave node of the mind map for Institution.
    In progress, not finished yet.
    
    (After traversing through 3 mind maps,  a few additional tests should be added to test_click.py to check the correctness of outputs.)
    
    see #86

--- tests/test_Institution.py
+++ tests/test_Institution.py
@@ -1,64 +1,47 @@
+import csv
 import unittest
 from datetime import date
+from pathlib import Path
 
 import pandas as pd
 
-from src.scripts.agent_table_processing import preparation, process_agent_tables
+from src.scripts.agent_table_processing import process_agent_tables
 from src.scripts.authenticity_institution import get_QID_inst
-from src.scripts.process_Institution import check_each_row_Inst, format_year_Inst
+from src.scripts.process_Institution import (
+    check_each_row_Inst,
+    format_year_Inst,
+    process_Inst,
+)
 
 
 class MyTestCase(unittest.TestCase):
+    # execute before every test case function run.
     def setUp(self):
-        self.inst_columns = [
-            "inst_id",
-            "inst_name",
-            "language",
-            "place",
-            # Here we use space_name directly. Be aware that we need to replace the space_id with space_name first in
-            # the tool
-            "start",
-            "end",
-            "alt_start",
-            "alt_end",
-            "inst_alt_name",
-            "source",
-            "page",
-            "created",
-            "created_by",
-            "last_modified",
-            "last_modified_by",
-            "wikidata_id",  # the original Institution table has no "wikidata_id" column, here we add one for
-            # convenience so that we don't need to get wikidata_id from agent table
-            "note",
-        ]
-        self.row_inst = [
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-        ]
-        self.inst = pd.DataFrame([self.row_inst])
-        self.inst.columns = self.inst_columns
-        self.inst = self.inst.astype(str)
+        THIS_DIR = Path(__file__).parent
+        ag_for_test = THIS_DIR / "ag_for_test.csv"
+        pl_for_test = THIS_DIR / "pl_for_test.csv"
+        inst_for_test = THIS_DIR / "inst_for_test.csv"
+        ag = pd.read_csv(ag_for_test).fillna("")
+        pl = pd.read_csv(pl_for_test).fillna("")
+        inst = pd.read_csv(inst_for_test).fillna("")
+
         self.today = date.today().strftime("%Y-%m-%d")
+        self.ag = ag
+        self.pl = pl
+        self.inst = inst.astype(str)
 
         self.all_agents_ids_gh = []
         self.last_inst_id = []
         self.all_agents_ids_gh = []
+        self.i = 0
+
+    # execute before every test case function run.
+    def tearDown(self):
+        del self.ag
+        del self.pl
+        del self.today
+        del self.inst
+        del self.i
 
     def test_it_should_return_QID_info(self):
         self.assertEqual(
@@ -72,122 +55,148 @@ class MyTestCase(unittest.TestCase):
             None,
         )
 
-    def test_it_should_add_infomation(self):
-        self.row_inst = [
-            "AG2000",
-            "Max Planck Society",
-            "en",
-            "",  # place
-            "1948",  # start
-            "",  # end
-            "",  # alt_start
-            "",  # alt_end
-            "",  # inst_alt_name
-            "",  # source
-            "",  # page
-            "2022-06-07",
-            "QG",
-            "",
-            "",
-            "Q158085",  # wikidata_id
-            "",  # note,
-        ]
-        self.inst.loc[0] = self.row_inst
-        self.all_agents_ids_gh = ["AG0002"]
-        self.last_inst_id = ["AG0002"]
+    def test_it_should_skip(self):
+        self.i = 0
+        row_for_test = self.inst.iloc[self.i]
+        self.all_agents_ids_gh = ["AG0001"]
+        self.last_inst_id = ["AG0001"]
         self.all_wikidata_ids = [""]
 
         row, last_inst_id = check_each_row_Inst(
-            0,
-            self.inst.iloc[0],
+            self.i,
+            row_for_test,
             self.inst,
             self.all_agents_ids_gh,
             self.last_inst_id,
             self.all_wikidata_ids,
         )
         # make the format of start and end (year) valid
-        row = format_year_Inst(row)
-        print("row: ", row)
-        # print(row.tolist()[0:-1])
+        row_new = format_year_Inst(row)
+        self.inst.loc[self.i] = row_new
+        self.inst.drop("note", axis=1, inplace=True)
         self.assertEqual(
-            row.tolist()[0:-1],
+            self.inst.iloc[self.i].tolist(),
             [
-                "AG2000",
-                "Max Planck Society",
-                "en",
-                "Munich",  # place
-                "1948",  # start
+                "AG5000",  # inst_id
+                "University of Tübingen",  # inst_name
+                "en",  # language
+                "Tübingen",  # place
+                "",  # start
                 "",  # end
                 "",  # alt_start
                 "",  # alt_end
-                "",  # inst_alt_name
+                "Eberhard Karl University of Tübingen",  # inst_alt_name
                 "",  # source
                 "",  # page
-                "2022-06-07",
-                "QG",
-                self.today,
-                "ReadActor",
-                "Q158085",
-            ],
+                # note is omitted
+                "2022-08-08",  # created
+                "QG",  # created_by
+                "",  # last_modified
+                "",  # last_modified_by
+                "Q153978",
+            ],  # wikidata_id
         )
 
-    def test_it_should_not_change_with_extended_year_format(self):
-        self.row_inst = [
-            "AG3000",
-            "University Germany",
-            "en",
-            "Germany",  # place
-            "197X",  # start
-            "",  # end
-            "",  # alt_start
-            "",  # alt_end
-            "",  # inst_alt_name
-            "",  # source
-            "",  # page
-            "2019-06-15",
-            "WH",
-            "",
-            "",
-            "",  # wikidata_id
-            "",  # note,
-        ]
-        self.inst.loc[0] = self.row_inst
-        self.all_agents_ids_gh = ["AG3000"]
-        self.last_inst_id = ["AG3000"]
+    def test_it_should_assert_error_because_no_instID(self):
+        self.i = 1
+        row_for_test = self.inst.iloc[self.i]
+        self.all_agents_ids_gh = ["AG0001"]
+        self.last_inst_id = ["AG0001"]
+        self.all_wikidata_ids = [""]
+
+        with self.assertRaises(SystemExit) as cm:
+            _, _ = check_each_row_Inst(
+                self.i,
+                row_for_test,
+                self.inst,
+                self.all_agents_ids_gh,
+                self.last_inst_id,
+                self.all_wikidata_ids,
+            )
+        self.assertEqual(cm.exception.code, None)
+
+    def test_it_should_add_start_year(self):
+        self.i = 2
+        row_for_test = self.inst.iloc[self.i]
+        self.all_agents_ids_gh = ["AG0001"]
+        self.last_inst_id = ["AG0001"]
         self.all_wikidata_ids = [""]
 
         row, last_inst_id = check_each_row_Inst(
-            0,
-            self.inst.iloc[0],
+            self.i,
+            row_for_test,
             self.inst,
             self.all_agents_ids_gh,
             self.last_inst_id,
             self.all_wikidata_ids,
         )
         # make the format of start and end (year) valid
-        row = format_year_Inst(row)
-        print("row: ", row)
-        # print(row.tolist()[0:-1])
+        row_new = format_year_Inst(row)
+        self.inst.loc[self.i] = row_new
+        self.inst.drop("note", axis=1, inplace=True)
         self.assertEqual(
-            row.tolist()[0:-1],
+            self.inst.iloc[self.i].tolist(),
             [
-                "AG3000",
-                "University Germany",
-                "en",
-                "Germany",  # place
-                "197X",  # start
+                "AG5000",  # inst_id
+                "University of Tübingen",  # inst_name
+                "en",  # language
+                "Tübingen",  # place
+                "1477",  # start
+                "",  # end
+                "",  # alt_start
+                "",  # alt_end
+                "Eberhard Karl University of Tübingen",  # inst_alt_name
+                "",  # source
+                "",  # page
+                # note is omitted
+                "2022-08-08",  # created
+                "QG",  # created_by
+                self.today,  # last_modified
+                "ReadActor",  # last_modified_by
+                "Q153978",
+            ],  # wikidata_id
+        )
+
+    def test_it_should_pass_without_change(self):
+        self.i = 3
+        row_for_test = self.inst.iloc[self.i]
+        self.all_agents_ids_gh = ["AG0001"]
+        self.last_inst_id = ["AG0001"]
+        self.all_wikidata_ids = [""]
+
+        row, last_inst_id = check_each_row_Inst(
+            self.i,
+            row_for_test,
+            self.inst,
+            self.all_agents_ids_gh,
+            self.last_inst_id,
+            self.all_wikidata_ids,
+        )
+        # make the format of start and end (year) valid
+        row_new = format_year_Inst(row)
+        self.inst.loc[self.i] = row_new
+        self.inst.drop("note", axis=1, inplace=True)
+        self.assertEqual(
+            self.inst.iloc[self.i].tolist(),
+            [
+                "AG5001",  # inst_id
+                "A made-up place",  # inst_name
+                "en",  # language
+                "",  # place
+                "",  # start
                 "",  # end
                 "",  # alt_start
                 "",  # alt_end
                 "",  # inst_alt_name
                 "",  # source
                 "",  # page
-                "2019-06-15",
-                "WH",
+                # note is omitted
+                "2022-08-08",  # created
+                "QG",  # created_by
+                "",  # last_modified
+                "",  # last_modified_by
                 "",
-                "",
-                "",
-            ],
+            ],  # wikidata_id
         )
 
 
